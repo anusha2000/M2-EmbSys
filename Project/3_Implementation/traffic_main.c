a7d57ad6db788b8fec1361d63d121af5f791d540
anusha2000 <anushapatil79@gmail.com>
2022-03-08 12:38:08 +0530

    commit

--- Project/3_Implementation/traffic_main.c
+++ Project/3_Implementation/traffic_main.c
@@ -10,7 +10,8 @@
  */
 
 #include "main.h"
-#define R1 PB0 
+// LED's are connected to PORTB AND PORTD
+#define R1 PB0
 #define Y1 PB1
 #define G1 PB2
 
@@ -27,72 +28,74 @@
 #define G4 PD5
 int main(void)
 {
-	DDRB = 0xff;
-	DDRD = 0xff;
-	DDRC = 0x00;
+    DDRB = 0xff; // PORTB is set as output by setting 0xff to this register
+    DDRD = 0xff; // PORTD is set as output by setting 0xff to this register
+    DDRC = 0x00; // PORTC is set as input by setting 0x00 this register
 
-	PORTB = 0x00;
-	PORTD = 0x00;
+    PORTB = 0x00; // PORTB is initialised as 0
+    PORTD = 0x00; // PORTD is initialised as 0
 
-	while (1)
-	{
-		if ((PINC & 0x01) == 0x01)
-		{
-			traffic_signal1();
-		}
-		else if ((PINC & 0x02) == 0x02)
-		{
-			traffic_signal2();
-		}
+    while (1)
+    {
+        if ((PINC & 0x01) == 0x01) // if PINC is equal to 0x01
+        {
+            traffic_signal1(); // call trffic_signal1 function
+        }
+        else if ((PINC & 0x02) == 0x02) // if PINC is equal to 0x02
+        {
+            traffic_signal2(); // call trffic_signal2 function
+        }
 
-		else if ((PINC & 0x04) == 0x04)
-		{
-			traffic_signal3();
-		}
+        else if ((PINC & 0x04) == 0x04) // if PINC is equal to 0x04
+        {
+            traffic_signal3(); // call trffic_signal3 function
+        }
 
-		else if ((PINC & 0x08) == 0x08)
-		{
-			traffic_signal4();
-		}
+        else if ((PINC & 0x08) == 0x08) // if PINC is equal to 0x08
+        {
+            traffic_signal4(); // call trffic_signal4 function
+        }
+        // if the above conditions are not true then the traffic signals should work on the time basis
+        else
+        {
+            //working of traffic signal during normal condition
+            PORTB = 0x00; // PORTB is initialised as 0
+            PORTD = 0x00; // PORTD is initialised as 0
 
-		else
-		{
-			PORTB = 0x00;
-			PORTD = 0x00;
+            PORTB |= (1 << G1);
+            PORTB |= (1 << Y2);
+            PORTD |= (1 << R3);
+            PORTD |= (1 << R4);
+            _delay_ms(30000); // delay
 
-			PORTB |= (1 << G1);
-			PORTB |= (1 << Y2);
-			PORTD |= (1 << R3);
-			PORTD |= (1 << R4);
-			_delay_ms(30000);
+            PORTB = 0x00; // PORTB is initialised as 0
+            PORTD = 0x00; // PORTD is initialised as 0
 
-			PORTB = 0x00;
-			PORTD = 0x00;
+            PORTB |= (1 << R1);
+            PORTB |= (1 << G2);
+            PORTD |= (1 << Y3);
+            PORTD |= (1 << R4);
+            _delay_ms(30000);  //delay
 
-			PORTB |= (1 << R1);
-			PORTB |= (1 << G2);
-			PORTD |= (1 << Y3);
-			PORTD |= (1 << R4);
-			_delay_ms(30000);
+            PORTB = 0x00;   // PORTB is initialised as 0
+            PORTD = 0x00;   // PORTD is initialised as 0
+            
+            PORTB |= (1 << R1);
+            PORTB |= (1 << R2);
+            PORTD |= (1 << G3);
+            PORTD |= (1 << Y4);
+            _delay_ms(30000);  //delay
 
-			PORTB = 0x00;
-			PORTD = 0x00;
-
-			PORTB |= (1 << R1);
-			PORTB |= (1 << R2);
-			PORTD |= (1 << G3);
-			PORTD |= (1 << Y4);
-			_delay_ms(30000);
-
-			PORTB = 0x00;
-			PORTD = 0x00;
-			PORTB |= (1 << Y1);
-			PORTB |= (1 << R2);
-			PORTD |= (1 << R3);
-			PORTD |= (1 << G4);
-			_delay_ms(30000);
-			PORTB = 0x00;
-			PORTD = 0x00;
-		}
-	}
+            PORTB = 0x00;  // PORTB is initialised as 0
+            PORTD = 0x00;  // PORTD is initialised as 0
+            
+            PORTB |= (1 << Y1);
+            PORTB |= (1 << R2);
+            PORTD |= (1 << R3);
+            PORTD |= (1 << G4);
+            _delay_ms(30000);  //delay
+            PORTB = 0x00;
+            PORTD = 0x00;
+        }
+    }
 }
